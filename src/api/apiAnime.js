bbdb8e445bb326b102834293dadd89ffc9d54e9b
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-06-24 15:12:54 -0300

    ajuste basico feito

--- src/api/apiAnime.js
+++ src/api/apiAnime.js
@@ -1,15 +1,19 @@
 import axios from 'axios'
 
 
+const local = axios.create({
+    baseURL:'http://localhost:5000'
+}) 
+
 export async function Postada(nome) {
-    const resposta = await axios.post('http://localhost:5000/anime' , {
+    const resposta = await local.post('/anime' , {
         nome:nome
     })
     return resposta;
 }
 
 export async function Getinho() {
-    const resposta = await axios.get('http://localhost:5000/animes')
+    const resposta = await local.get('/animes')
     return resposta.data;
 }
 
